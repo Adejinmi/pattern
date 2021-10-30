ec96e8027fabc87bde7d5767fa6286457e65aed1
Samuel Adejinmi <adejinmisamuel1@gmail.com>
2021-10-30 23:43:56 +0100

    make it better

--- pages/_document.js
+++ pages/_document.js
@@ -1,46 +1,30 @@
-import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default class Document extends NextDocument {
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-				});
-			const initialProps = await NextDocument.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link
-						href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,100;1,600&display=swap'
-						rel='stylesheet'
-					/>
-					<link href='https://fonts.googleapis.com/css2?family=Domine&display=swap' rel='stylesheet'></link>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 }
